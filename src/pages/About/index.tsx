6cde7e6d1f0ea2dc41fbc7fbb21ea5f09c6a5fe9
Wellington Torquato <torquatojucas@gmail.com>
2024-06-12 20:18:26 -0300

    fixed readme description project

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -15,7 +15,7 @@ export function About() {
             <a href="https://github.com/EmanuelQuintino/Task-Manager-API" target="_blank">
               Node.js
             </a>
-            , Usando o conceito de SPA (Single Page Application) e a arquitetura RESTful
+            . Usando o conceito de SPA (Single Page Application) e a arquitetura RESTful
             API (Representational State Transfer), para a conclusão do Curso de
             Desenvolvimento Web ofertado pelo{" "}
             <a href="https://emanuelquintino.github.io/Page-WDC/" target="_blank">
